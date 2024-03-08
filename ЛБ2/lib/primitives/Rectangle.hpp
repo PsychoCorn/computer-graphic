87c74fe0a0f11b9e390e122e18f4bfe43d93b234
PsychoCorn <icetime.artem@gmail.com>
2024-03-08 17:25:54 +0800

    Прототип ко второй лабе

--- ЛБ2/lib/primitives/Rectangle.hpp
+++ ЛБ2/lib/primitives/Rectangle.hpp
@@ -3,11 +3,12 @@
 
 #include "../Drawable.hpp"
 #include "../Color.hpp"
+#include <array>
 
 namespace kondraLib
 {
 
-    class Rectangle : Drawable
+    class Rectangle :public Drawable
     {
     protected:
         GLfloat _x;
@@ -15,6 +16,7 @@ namespace kondraLib
         GLfloat _height;
         GLfloat _width;
         Color _color;
+        std::array<std::pair<GLfloat, GLfloat>, 4> _vertices;
 
     public:
         Rectangle(const GLfloat &, const GLfloat &, const GLfloat &, const GLfloat &, const Color & = Color::BLACK);
@@ -24,16 +26,22 @@ namespace kondraLib
 
     Rectangle::Rectangle(const GLfloat &x, const GLfloat &y, const GLfloat &width,
                         const GLfloat &height, const Color &color) : _x(x), _y(y), _height(height), 
-                                                                    _width(width), _color(color) {}
+                                                                    _width(width), _color(color) 
+    {
+        _vertices[0] = {_x, _y};
+        _vertices[1] = {_x + _width, _y};
+        _vertices[2] = {_x + _width, _y + _height};
+        _vertices[3] = {_x, _y + _height};
+    }
 
     void Rectangle::draw()
     {
         glColor4f(_color.getColor().red, _color.getColor().green, _color.getColor().blue, _color.getColor().alpha);
         glBegin(GL_QUADS);
-        glVertex2f(_x, _y);
-        glVertex2f(_x + _width, _y);
-        glVertex2f(_x + _width, _y + _height);
-        glVertex2f(_x, _y + _height);
+        for (auto&& [x, y] : _vertices)
+        {
+            glVertex2f(x, y);
+        }
         glEnd();
     }
 
