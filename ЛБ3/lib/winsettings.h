c41abf4f018ba9508adfe3288be875d3b5f82a66
PsychoCorn <icetime.artem@gmail.com>
2024-05-01 18:18:03 +0800

    ЛР3.1

--- ЛБ3/lib/winsettings.h
+++ ЛБ3/lib/winsettings.h
@@ -6,7 +6,7 @@
 const int32_t WIN_WIDTH = 1280;
 const int32_t WIN_HEIGHT = 720;
 const char WIN_TITLE[] = "Frog Game";
-const DWORD sleep_time = 1000 / 20;
+const DWORD sleep_time = 1000 / 60;
 
 HWND hwnd;
 
