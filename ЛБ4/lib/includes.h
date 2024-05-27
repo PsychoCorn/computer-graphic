00f5564b941a513ab7a2a1a3ef4cfef7dc66138b
PsychoCorn <icetime.artem@gmail.com>
2024-05-27 11:17:20 +0800

    moved to cb

--- ЛБ4/lib/includes.h
+++ ЛБ4/lib/includes.h
@@ -7,10 +7,7 @@
 #include <gl/gl.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "../stb-master/stb_image.h"
-#include "../stb-master/stb_easy_font.h"
-#include "../stb-master/stb_textedit.h"
-#include "../stb-master/stb_truetype.h"
-#include "../stb-master/stb_rect_pack.h"
 #include <inttypes.h>
+#include <stdbool.h>
 
 #endif
